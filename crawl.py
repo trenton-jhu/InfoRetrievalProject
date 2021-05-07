b40e400e9fb005223f4aff4e9f8ec5eaf4840b6d
trenton-uima <ywang382@jhu.edu>
2021-05-07 19:09:04 -0400

    update crawler to extract movies of different genres

--- crawl.py
+++ crawl.py
@@ -1,85 +1,109 @@
 import json
+import argparse
 from queue import Queue
 
 from urllib import parse, request
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0'}
-base = "https://www.imdb.com/title/"
+
+class MovieCrawler:
+    """
+    Crawler for IMDB movie home page
+    """
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    base = "https://www.imdb.com/title/"
+
+    def __init__(self):
+        self.visited = set()
+        self.crawled = []
+        self.result = []
+
+    def output(self, data_file="movies.json", visited_file="visited.json"):
+        with open(data_file, 'w') as outfile:
+            json.dump(self.result, outfile, indent=2)
+        with open(visited_file, 'w') as outfile:
+            json.dump(self.crawled, outfile, indent=2)
+
+    def crawl(self, root, max_links=100, genre="Action"):
+        queue = Queue()
+        queue.put(root)
+        count = 0
+        while not queue.empty():
+            if count >= max_links:
+                break
+            link = queue.get()
+            if link in self.visited:
+                continue
+            if count % 100 == 0:
+                print(f"Crawled {count} pages.")
+
+            try:
+                page, links = self.extract_info(link, genre)
+                count += 1
+                self.crawled.append(self.base + link)
+                self.result.append(page)
+                self.visited.add(link)
+                for x in links:
+                    queue.put(x)
+            except Exception as _:
+                self.visited.add(link)
+
+    def extract_info(self, link, genre="Action"):
+        data = {"id": link}
+        url = self.base + link
+        data["url"] = url
+        req = request.Request(url, headers=self.headers)
+        soup = BeautifulSoup(request.urlopen(req), 'html.parser')
+
+        subtext = soup.find("div", {'class': 'subtext'})
+        genres = [i.text.strip() for i in subtext.contents
+                  if len(i.string.strip()) > 3 and i.string.strip().isalpha()]
+        if genre not in genres:
+            raise Exception("Skipping because different genre")
+
+        title = soup.find("div", {'class': 'titleBar'}).find("h1")
+        data["name"] = title.contents[0].replace(u'\xa0', u'')
+
+        image = soup.find("div", {'class': 'poster'}).find("img")
+        data["image"] = image['src']
+
+        data["genre"] = genres
+
+        summary = soup.find("div", {'class': 'summary_text'})
+        data["summary"] = summary.text.strip()
+
+        credit = soup.find_all("div", {'class': 'credit_summary_item'})[0]
+        data["director"] = [x.text for x in credit.find_all("a")]
+
+        storyline = soup.find("div", {'id': 'titleStoryLine', 'class': 'article'})
+        data['storyline'] = storyline.find_all("span")[1].text.strip()
+
+        cast_list = soup.find("table", {'class': 'cast_list'}).find_all("a")
+        data['cast'] = [x.text.strip() for x in cast_list if x.text and x['href'].startswith("/name/")]
+
+        hrefs = [x.find("a") for x in soup.find_all("div", {'class': 'rec_item'})]
+        recs = [x['href'].split("/")[2] for x in hrefs]
+
+        runtime = soup.find("time")['datetime']
+        minutes = ''.join([i for i in runtime if i.isdigit()])
+        data['runtime'] = int(minutes)
+
+        rating = soup.find("span", {"itemprop": "ratingValue"})
+        data["rating"] = float(rating.string)
+
+        num_rating = soup.find("span", {"itemprop": "ratingCount"})
+        data["ratingCount"] = int(num_rating.string.replace(',', ''))
+
+        return data, recs
 
 
-def extract_info(url):
-    data = {}
-    req = request.Request(url, headers=headers)
-    soup = BeautifulSoup(request.urlopen(req), 'html.parser')
-
-    rating = soup.find("span", {"itemprop": "ratingValue"})
-    data["rating"] = float(rating.string)
-
-    num_rating = soup.find("span", {"itemprop": "ratingCount"})
-    data["ratingCount"] = int(num_rating.string.replace(',', ''))
-
-    title = soup.find("div", {'class': 'titleBar'}).find("h1")
-    data["name"] = title.contents[0].replace(u'\xa0', u'')
-
-    genre = soup.find("div", {'class': 'subtext'})
-    data["genre"] = [i.text.strip() for i in genre.contents
-                     if len(i.string.strip()) > 3 and i.string.strip().isalpha()][0]
-
-    summary = soup.find("div", {'class': 'summary_text'})
-    data["summary"] = summary.text.strip()
-
-    credit = soup.find_all("div", {'class': 'credit_summary_item'})[0]
-    data["director"] = [x.text for x in credit.find_all("a")]
-
-    storyline = soup.find("div", {'id': 'titleStoryLine', 'class': 'article'})
-    data['storyline'] = storyline.find_all("span")[1].text.strip()
-
-    cast_list = soup.find("table", {'class': 'cast_list'}).find_all("a")
-    data['cast'] = [x.text.strip() for x in cast_list if x.text and x['href'].startswith("/name/")]
-
-    hrefs = [x.find("a") for x in soup.find_all("div", {'class': 'rec_item'})]
-    recs = [x['href'].split("/")[2] for x in hrefs]
-
-    runtime = soup.find("time")['datetime']
-    minutes = ''.join([i for i in runtime if i.isdigit()])
-    data['runtime'] = int(minutes)
-
-    return data, recs
-
-
-def crawl(root, max_links=100):
-    queue = Queue()
-    queue.put(root)
-    visited = set()
-    data = []
-
-    while not queue.empty():
-        if len(visited) >= max_links:
-            break
-        link = queue.get()
-        if link in visited:
-            continue
-        else:
-            visited.add(link)
-        if len(visited) % 10 == 0:
-            print(f"Crawled {len(visited)} pages.")
-
-        try:
-            page, links = extract_info(base + link)
-            data.append(page)
-            for x in links:
-                queue.put(x)
-        except Exception as e:
-            print(e, base + link)
-
-    urls = [base + x for x in visited]
-    return data, urls
+def main():
+    crawler = MovieCrawler()
+    crawler.crawl("tt1345836", 1000, "Action")
+    crawler.crawl("tt2674426", 1000, "Romance")
+    crawler.crawl("tt0109686", 1000, "Comedy")
+    crawler.output()
 
 
 if __name__ == '__main__':
-    data, visited = crawl("tt1345836", 1)
-    with open('movies.json', 'w') as outfile:
-        json.dump(data, outfile, indent=2)
-    with open('visited.json', 'w') as outfile:
-        json.dump(visited, outfile, indent=2)
+    main()
