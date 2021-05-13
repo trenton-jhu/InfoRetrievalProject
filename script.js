5a20608b0a8f452f8a9edc2589d06e1dae05c98a
trenton-uima <ywang382@jhu.edu>
2021-05-12 22:46:18 -0400

    begin flask server and web interface impl

---
+++ script.js
@@ -0,0 +1,87 @@
+function handleSubmit() {
+    const form = document.getElementById("form-genre");
+    const note = document.getElementById("genre-result");
+    note.innerText = "Classifying ... Please wait!";
+    const query = form[0].value;
+
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://127.0.0.1:5000/genre?data=' + query);
+    request.onload = () => {
+        const result = JSON.parse(request.response);
+        if (request.status === 200) {
+            note.style.display = 'block';
+            note.innerText = "Predicted genre for your plot: " + result.genre;
+        }
+    };
+    request.send();
+}
+
+function handleSubmit2() {
+    const form = document.getElementById("form-genres");
+    const note = document.getElementById("genres-result");
+    note.innerText = "Classifying ... Please wait!";
+    const query = form[0].value;
+
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://127.0.0.1:5000/genres?data=' + query);
+    request.onload = () => {
+        const result = JSON.parse(request.response);
+        if (request.status === 200) {
+            note.style.display = 'block';
+            note.innerText = "Predicted genre for your plot: " + result.genres;
+        }
+    };
+    request.send();
+}
+
+function handleInit(type) {
+    document.getElementById("init").style.display = "none";
+    document.getElementById("feedback").style.display = "block";
+    document.getElementById("like").onclick = () => handleFeedback(0);
+    document.getElementById("dislike").onclick = () => handleFeedback(1);
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://127.0.0.1:5000/init?mode=' + type);
+    request.onload = () => {
+        const result = JSON.parse(request.response);
+        if (request.status === 200) {
+            displayMovies(result);
+        }
+    };
+    request.send();
+}
+
+function handleFeedback(label) {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://127.0.0.1:5000/feedback?data=' + label);
+    request.onload = () => {
+        const result = JSON.parse(request.response);
+        if (request.status === 200) {
+            displayMovies(result);
+        }
+    };
+    request.send();
+}
+
+function displayMovies(data) {
+    const note = document.getElementById("prediction");
+    if (data.prediction === 0) {
+        note.innerText = "We predict that you would like this movie!";
+    } else {
+        note.innerText = "We predict that you would not like this movie!"
+    }
+    document.getElementById("poster").src = data.image;
+    document.getElementById("imdb").href = data.url;
+    document.getElementById("title").innerText = data.title;
+    document.getElementById("director").innerText = data.director;
+    document.getElementById("cast").innerText = data.cast;
+    document.getElementById("runtime").innerText = data.runtime + " min";
+    document.getElementById("summary").innerText = data.summary;
+    document.getElementById("movie-card").style.display = "block";
+}
+
+
+document.getElementById("submit").onclick = handleSubmit;
+document.getElementById("submit2").onclick = handleSubmit2;
+document.getElementById("nearest-centroid").onclick = () => handleInit("nearest");
+document.getElementById("knn").onclick = () => handleInit("knn");
+
---
+++ server.py
@@ -0,0 +1,81 @@
+import flask
+import json
+from flask import request, jsonify
+from flask_cors import CORS
+
+from helpers import *
+from feedback import *
+
+# Flask server init
+app = flask.Flask(__name__)
+CORS(app)
+app.config["DEBUG"] = False
+
+# Load trained classifiers
+sclf = SingleLabelClassifier()
+sclf.load_clf("best_single_label_clf.pkl")
+mclf = MultiLabelClassifier()
+mclf.load_clf("best_multi_label_clf.pkl")
+
+# Load movie data and term vectors
+df = pd.read_csv('term_vectors.csv')
+df = df.sample(frac=1).reset_index(drop=True)
+state = {}
+
+
+def get_movie(index):
+    term_vector = eval(df.iloc[index]['tfidf'])
+    movie = {
+        'title': df.iloc[index]['name'],
+        'url': df.iloc[index]['url'],
+        'image': df.iloc[index]['image'],
+        'genre': df.iloc[index]['genre'],
+        'director': df.iloc[index]['director'],
+        'cast': df.iloc[index]['cast'],
+        'summary': df.iloc[index]['storyline'],
+        'runtime': int(df.iloc[index]['runtime']),
+        'prediction': state['feedback'].predict_label(term_vector),
+    }
+    return jsonify(movie)
+
+
+@app.route('/genre', methods=['GET'])
+def predict_genre():
+    data = request.args.get("data")
+    genre = sclf.predict_one(pd.DataFrame({
+        'storyline': data
+    }, index=[0]))
+    return jsonify({'genre': genre})
+
+
+@app.route('/genres', methods=['GET'])
+def predict_genres():
+    data = request.args.get("data")
+    genres = mclf.predict_one(pd.DataFrame({
+        'storyline': data
+    }, index=[0]))
+    return jsonify({'genres': genres})
+
+
+@app.route('/init', methods=['GET'])
+def init_feedback():
+    data = request.args.get("mode")
+    if data == 'nearest':
+        state['feedback'] = NearestCentroidRelevanceFeedback()
+    else:
+        state['feedback'] = NearestCentroidRelevanceFeedback()
+    state['index'] = 0
+    return get_movie(state['index'])
+
+
+@app.route('/feedback', methods=['GET'])
+def step_feedback():
+    data = request.args.get("data")
+    term_vector = eval(df.iloc[state['index']]['tfidf'])
+    state['feedback'].add_label(term_vector, int(data))
+    state['index'] += 1
+    return get_movie(state['index'])
+
+
+if __name__ == '__main__':
+    app.run()
